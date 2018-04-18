1ca246461a7fb0270537dc51bc035d5928fea645
Michael Irvine <michael.f.irvine85@gmail.com>
2018-04-17 17:48:23 -0700

    working on main menu

--- js/sassenscript.js
+++ js/sassenscript.js
@@ -1,28 +1,41 @@
-jQuery(document).ready(function($){
-
-
-// Desktop menu hide on scroll 
-$(window).scroll(function() {
-    var y_scroll_pos    = window.pageYOffset;
-    var scrollPosition  = 350;
+jQuery(document).ready(function($) {
+  // Desktop menu hide on scroll
+  $(window).scroll(function() {
+    var y_scroll_pos = window.pageYOffset;
+    var scrollPosition = 250;
     var $menuList = $(".menu-item-23, .menu-item-24, .menu-item-25");
 
     // Hides menu when viewer scrolls down
-    if(y_scroll_pos > scrollPosition) {
-	   $menuList.addClass("hide-menu");
-    }else {
+    if (y_scroll_pos > scrollPosition) {
+      $menuList.addClass("hide-menu");
+    } else {
       $menuList.removeClass("hide-menu");
     }
 
-    // 
-    if( y_scroll_pos > scrollPosition ) {
-        $('.line-one, .line-two').click(function(){
-         $menuList.toggleClass('show-menu');   
-        });    
-    }else {
-        return;
+    // Makes the Menu lines move 
+
+    // ----------- Top line ----------------------------
+    if (y_scroll_pos > scrollPosition) {
+      $(".line-one").addClass("line-one-animate");
+    } else {
+      $(".line-one").removeClass("line-one-animate");
     }
 
-    });
+    // ----------- Bottom line --------------------------
+    if (y_scroll_pos > scrollPosition) {
+      $(".line-two").addClass("line-two-animate");
+    } else {
+      $(".line-two").removeClass("line-two-animate");
+    }
 
+    //
+    if (y_scroll_pos > scrollPosition) {
+       $('.line-one, .line-two').click(function(){
+        $menuList.toggleClass('show-menu');
+       }); 
+    } else {
+        return;
+        // Items won't be clickable
+    }
+  });
 });
